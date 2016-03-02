f4eb4402ca94a54e82854f59b4d6d90e858a1049
Stef Vartolomeev <stef.vartolomeev@gmail.com>
2016-03-02 12:45:50 +0200

    Complex Rationals: Version 3 - complex rationals

--- ex2ComplexRationals.cpp
+++ ex2ComplexRationals.cpp
@@ -4,6 +4,15 @@ using namespace std;
 // input: 2 rational numbers, output: their sum
 // e.g. 1/3, 2/5 -> 11/15
 
+void sum(
+      // число 1
+      int n1nom, int n1denom, int n1inom, int n1idenom
+      // число 2
+      int n2nom, int n2denom, int n2inom, int n2idenom
+      // резултат
+      int & resultNom, int & resultDenom, int & resultiNom, int & resultiDenom) {
+  // ...
+}
 
 void sum(int n1nom, int n1denom,
          int n2nom, int n2denom,
@@ -14,31 +23,45 @@ void sum(int n1nom, int n1denom,
 }
 
 int main() {
-  // число 1
+   // число 1
   int n1nom;
   int n1denom;
+  int n1inom;
+  int n1idenom;
 
   // число 2
   int n2nom;
   int n2denom;
+  int n2inom;
+  int n2idenom;
 
   // резултат
   int resultNom;
   int resultDenom;
+  int resultiNom;
+  int resultiDenom;
 
   cout << "Enter n1: ";
   cin >> n1nom;
   cin >> n1denom;
+  cin >> n1inom;
+  cin >> n1idenom;
 
   cout << "Enter n2: ";
   cin >> n2nom;
   cin >> n2denom;
+  cin >> n2inom;
+  cin >> n2idenom;
 
-  sum(n1nom, n1denom, n2nom, n2denom,
-      resultNom, resultDenom);
+  sum(n1nom, n1denom, n1inom, n1idenom,
+      n2nom, n2denom, n2inom, n2idenom,
+      resultNom, resultDenom, resultiNom, resultiDenom);
 
   cout << "Result: "
        << resultNom << "/" << resultDenom
+       << " + i("
+       << resultiNom << "/" << resultiDenom
+       << ")"
        << endl;
   return 0;
 }
