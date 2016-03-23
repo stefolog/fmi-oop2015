9275e1cc9d7a981195fd905386b5ed3498563169
Stef Vartolomeev <stef.vartolomeev@gmail.com>
2016-03-23 12:02:01 +0200

    Ex5: solution to problem 1

--- ex5/ex5.cpp
+++ ex5/ex5.cpp
@@ -3,7 +3,42 @@
 
 using namespace std;
 
+class Person {
+public:
+
+  void read() {
+    char buf[20];
+    cout << "Enter name, age: ";
+    cin.getline(name, 31, ',');
+    // TODO: add buf to read till ',' for longer text
+    cin >> age;
+  }
+
+  void print() const {
+    cout << name << " (" << age << ")" << endl;
+  }
+
+  int getAge() const {
+    return age;
+  }
+
+private:
+  char name[31];
+  int age;
+};
+
 int main() {
-  Decimal(5, 3).print();
+  // Decimal(5, 3).print();
+
+  Person people[10];
+
+  for (int i = 0; i < 2; i++) {
+    people[i].read();
+  }
+
+  for (int i = 0; i < 2; i++) {
+    people[i].print();
+  }
+
   return 0;
 }
